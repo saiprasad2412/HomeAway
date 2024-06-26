6b9a39ae0d21a72b620dd66aa5ab8a031d8c3f53
saiprasad2412 <manesaiprasad2412@gmail.com>
2024-06-26 18:10:48 +0530

    india cordinate added

--- client/src/components/map/Map.jsx
+++ client/src/components/map/Map.jsx
@@ -7,7 +7,7 @@ function Map({items}){
   return (
     <MapContainer center={items.length===1?
     [items[0].latitude,items[0].longitude]
-    :[52.4797, -1.90269]} zoom={7} scrollWheelZoom={false} className='map'>
+    :[21.7679, 78.8718]} zoom={5} scrollWheelZoom={false} className='map'>
     <TileLayer
       attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
       url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
